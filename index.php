040d938e878771608de4710d7e67e95c304696e7
Renata27 <r.imaculada27@gmail.com>
2017-08-22 11:56:46 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+  <p class="Title_css"> PPT Games        </p>
+          
+	    
+          <?php
+ if ($handle = opendir('./content/Games')) {
+   while (false !== ($file = readdir($handle)))
+      {
+          if ($file != "." && $file != "..")
+	  {
+          	$thelist .= '<li class="body_css"><a class="body_css" href="/content/Games/'.$file.'">'.$file.'</a></li>';
+          }
+       }
+  closedir($handle);
+  }
+?>
+<ul><?=$thelist?></ul>
+<?php
+unset($thelist);
+?>
